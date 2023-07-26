8d90b190a8e8a0100b8f089f95e56c7defb705f4
iwillens <iwillens@student.42heilbronn.de>
2023-07-26 20:25:09 +0000

    parser done

---
+++ srcs/print/help.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   help.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwillens <iwillens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 14:43:19 by iwillens          #+#    #+#             */
+/*   Updated: 2023/07/26 12:46:30 by iwillens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ping.h"
+
+void	print_help(t_ping *ft_ping)
+{
+	t_lstopt	*opt;
+	size_t		i;
+	char		buf[64];
+
+	i = 0;
+	opt = ft_ping->options.available;
+	dprintf(STDOUT_FILENO, HPL_USAGE, ft_ping->program);
+	dprintf(STDOUT_FILENO, HPL_GOAL);
+	dprintf(STDOUT_FILENO, HPL_OPTIONS);
+	while (i < OPT_LSTSIZE)
+	{
+		ft_strcpy(buf, opt[i].fullname);
+		if (opt[i].type != OPTT_NULL)
+		{
+			ft_strcpy(buf + ft_strlen(buf), "=");
+			ft_strcpy(buf + ft_strlen(buf), tname(opt[i].type));
+		}
+		dprintf(STDOUT_FILENO, "  -%c, --%-21s %s\n",
+			opt[i].shortcut, buf, opt[i].desc);
+		i++;
+	}
+	dprintf(STDOUT_FILENO, "\n%s %s\n", HPL_ARGS, HPL_ARGS2);
+	exit(0);
+}
+
+void	print_version(t_ping *ft_ping)
+{
+	dprintf(STDOUT_FILENO, VRS, ft_ping->program, 0, UINT_MAX);
+	dprintf(STDOUT_FILENO, VRS1);
+	dprintf(STDOUT_FILENO, VRS2);
+	dprintf(STDOUT_FILENO, VRS3);
+	dprintf(STDOUT_FILENO, VRS4);
+	dprintf(STDOUT_FILENO, VRS5);
+	exit(0);
+}
---
+++ srcs/print/usage.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   usage.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwillens <iwillens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 09:08:03 by iwillens          #+#    #+#             */
+/*   Updated: 2023/07/26 13:57:16 by iwillens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ping.h"
+
+
+static int	prt_options(t_ping *ft_ping, int kind, int used, int preceeding)
+{
+	size_t		i;
+	t_lstopt	*opt;
+
+	i = 0;
+	opt = ft_ping->options.available;
+	if (kind == OPTT_NULL)
+		used += dprintf(STDERR_FILENO, "%s", " -");
+	while (i < OPT_LSTSIZE)
+	{
+		if (used >= 70)
+			used = dprintf(STDERR_FILENO, "\n%*s", preceeding, "") - 1;
+		if (kind == OPRT_SHORTCUT && opt[i].type == OPTT_NULL)
+			used += dprintf(STDERR_FILENO, "%c", opt[i].shortcut);
+		else if (kind == OPRT_DSHORTCUT && opt[i].type != OPTT_NULL)
+			used += dprintf(STDERR_FILENO, " [-%c %s]",
+					opt[i].shortcut, tname(opt[i].type));
+		else if (kind == OPRT_FULLNAME && opt[i].type == OPTT_NULL)
+			used += dprintf(STDERR_FILENO, " [--%s]", opt[i].fullname);
+		else if (kind == OPRT_DFULLNAME && opt[i].type != OPTT_NULL)
+			used += dprintf(STDERR_FILENO, " [--%s=%s]",
+					opt[i].fullname, tname(opt[i].type));
+		i++;
+	}
+	return (used);
+}
+
+char	*tname(int type)
+{
+	if (type == OPTT_SHORT)
+		return (OSTR_SHORT);
+	else if (type == OPTT_LONG)
+		return (OSTR_LONG);
+	else if (type == OPTT_PATTERN)
+		return (OSTR_PATTERN);
+	return (OSTR_NULL);
+}
+
+void	print_usage(t_ping *ft_ping)
+{
+	int			preceeding;
+	int			used;
+
+	preceeding = dprintf(STDERR_FILENO, "Usage: %s", ft_ping->program);
+	used = preceeding;
+	used = prt_options(ft_ping, OPRT_SHORTCUT, used, preceeding);
+	used = prt_options(ft_ping, OPRT_DSHORTCUT, used, preceeding);
+	used = prt_options(ft_ping, OPRT_FULLNAME, used, preceeding);
+	used += prt_options(ft_ping, OPRT_DFULLNAME, used, preceeding);
+	dprintf(STDERR_FILENO, " HOST ...\n");
+	exit(0);
+}
+
+void	print_shortusage(t_ping *ft_ping)
+{
+	dprintf(STDERR_FILENO, ERR_SRTUSAGE, ft_ping->program, ft_ping->program);
+}
