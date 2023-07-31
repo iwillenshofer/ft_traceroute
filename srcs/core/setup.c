bad14560bfb379bfcc1332d3273162a0742b994c
Igor Willenshofer <iwillens@student.42sp.org.br>
2023-07-31 17:40:28 +0200

    final

---
+++ srcs/core/setup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setup.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwillens <iwillens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/30 14:42:53 by iwillens          #+#    #+#             */
+/*   Updated: 2023/07/31 16:48:19 by iwillens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** this file sets up the ft_ping structure.
+*/
+
+#include "ft_ping.h"
+
+static void	set_defaults(t_ping *ft_ping)
+{
+	ft_ping->opts.size = DFL_PACKET_SIZE;
+	ft_ping->opts.interval.tv_sec = 1;
+}
+
+/*
+** after parsing, set the options
+*/
+static void	set_options(t_ping *ft_ping)
+{
+	if (ft_ping->opts.flood)
+	{
+		ft_ping->opts.interval.tv_sec = 0;
+		ft_ping->opts.interval.tv_usec = DFL_FLOOD_RATE;
+	}
+}
+
+void	setup(t_ping *ft_ping, char **argv)
+{
+	ft_bzero(ft_ping, sizeof(t_ping));
+	add_options(ft_ping);
+	set_defaults(ft_ping);
+	parse(ft_ping, argv);
+	set_options(ft_ping);
+	ft_ping->pid = (unsigned short)getpid();
+}
---
+++ srcs/core/socket.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   socket.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwillens <iwillens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/27 13:02:27 by iwillens          #+#    #+#             */
+/*   Updated: 2023/07/31 14:42:58 by iwillens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_ping.h>
+
+/*
+** References:
+** https://linux.die.net/man/7/socket
+** https://linux.die.net/man/7/ip
+** https://linux.die.net/man/2/setsockopt
+*/
+
+void	opensocket(t_ping *ft_ping)
+{
+	int	fd;
+	int	vtrue;
+
+	vtrue = 1;
+	fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (fd < 0)
+		prs_fatal(ft_ping, ERR_SOCKET, NULL, false);
+	setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &vtrue, sizeof(vtrue));
+	if (ft_ping->opts.ttl && setsockopt(fd, SOL_IP, IP_TTL,
+			&(ft_ping->opts.ttl), sizeof(ft_ping->opts.ttl)))
+		dprintf(STDERR_FILENO, ERR_SCKOPTTTL, ft_ping->program);
+	ft_ping->sock = fd;
+}
