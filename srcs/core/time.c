d28d9e0b428f7587a7ba2bd5020436059e90ac34
Igor Willenshofer <iwillens@student.42sp.org.br>
2023-08-10 22:51:00 +0200

    done and done

---
+++ srcs/core/time.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwillens <iwillens@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 09:12:12 by iwillens          #+#    #+#             */
+/*   Updated: 2023/08/09 20:38:46 by iwillens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_traceroute.h"
+
+/*
+** returns a timeval with the difference between begin and end.
+*/
+static t_time	elapsed_time(t_time begin, t_time end)
+{
+	int		nsec;
+	t_time	diff;
+
+	if (end.tv_usec < begin.tv_usec)
+	{
+		nsec = (begin.tv_usec - end.tv_usec) / 1000000 + 1;
+		begin.tv_usec -= 1000000 * nsec;
+		begin.tv_sec += nsec;
+	}
+	if (end.tv_usec - begin.tv_usec > 1000000)
+	{
+		nsec = (end.tv_usec - begin.tv_usec) / 1000000;
+		begin.tv_usec += 1000000 * nsec;
+		begin.tv_sec -= nsec;
+	}
+	diff.tv_sec = end.tv_sec - begin.tv_sec;
+	diff.tv_usec = end.tv_usec - begin.tv_usec;
+	return (diff);
+}
+
+/*
+** returns the elapsed time in ms, up to (INT_MAX) ms,
+** which would result in 2147483 secs, or 35791 minutes or 596
+** hours before overflowing.
+** Overflow will not be handled.
+*/
+double	elapsed_time_ms(t_time begin, t_time end)
+{
+	t_time	elapsed;
+
+	elapsed = elapsed_time(begin, end);
+	return ((double)elapsed.tv_sec * 1000 + (double)elapsed.tv_usec / 1000);
+}
+
+t_time	ms_to_time(double ms)
+{
+	t_time	t;
+
+	t.tv_sec = ms / 1000;
+	t.tv_usec = (ms - (double)(t.tv_sec * 1000.0)) * 1000.0;
+	return (t);
+}
+
+/*
+** returns whether the elapsed time between begin and now
+** is greater than timeout
+*/
+t_bool	timed_out(t_time begin, t_time timeout)
+{
+	t_time	now;
+	t_time	elapsed;
+
+	gettimeofday(&now, NULL);
+	elapsed = elapsed_time(begin, now);
+	if (elapsed.tv_sec > timeout.tv_sec)
+		return (true);
+	else if (elapsed.tv_sec < timeout.tv_sec)
+		return (false);
+	else if (elapsed.tv_usec >= timeout.tv_usec)
+		return (true);
+	return (false);
+}
